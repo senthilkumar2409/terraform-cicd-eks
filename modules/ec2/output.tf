14d621d512cd9014deb265aa2b95f3b3dd036e48
Senthilkumar2409 <senthil24091999@gmail.com>
2024-08-09 12:56:11 +0000

    updated tf files

--- modules/ec2/output.tf
+++
@@ -1,5 +0,0 @@
-/*
-output "instance_id" {
-  value = [ for ins in aws_instance.terraform_prod : ins.id ]
-}
-*/
--- modules/vpc/output.tf
+++
@@ -1,8 +0,0 @@
-output "aws_vpc_id" {
-  value = aws_vpc.terraform_prod.id
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public_subnet.*.id
-}
-
